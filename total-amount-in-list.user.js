d8f5158de88fb7e6f87d8bf8ffb664196d5f9643
ZEROssk <a.k.jack.ssk@gmail.com>
2021-12-03 00:47:02 +0900

    add ReCheck button & new style

--- total-amount-in-list.user.js
+++ total-amount-in-list.user.js
@@ -10,7 +10,16 @@
 // @updateURL	https://github.com/ZEROssk/Total-amount-in-AmazonList/raw/master/total-amount-in-list.user.js
 // ==/UserScript==
 
-(function() {
+var reCheck = function() {
+	let clearContent = document.getElementById(
+		'tACheck'
+	);
+	clearContent.remove();
+
+	totalAmount();
+};
+
+var totalAmount = function() {
 	'use strict';
 
 	let insertPosition = document.getElementsByClassName(
@@ -26,8 +35,23 @@
 	}
 
 	let insertHTML =
-		'<div style="font-size:200%; margin-top:5%;">Products : '+products.length+' , Total Amount : '+tAmount+'</div>'
+		`<div id="tACheck" style="margin-top:5%; display: flex;">` +
+			`<div id="totalAmount" style="font-size: 170%; color: #0099CC;">` +
+				`<p>Products : `+products.length+` , Total Amount : `+tAmount+` <<</p>` +
+			`</div>` +
+			`<div id="reCheckButton">` +
+				`<button id="reCheckTA" style="margin-left: 1em; width: 4em; border-radius: 100vh; border: 0; background: #1aa1ff; color: #FFF;">Re</button>` +
+			`</div>` +
+		`</div>`
 	;
 	insertPosition[0].insertAdjacentHTML('afterend', insertHTML);
-})();
 
+	let btEvent = document.getElementById("reCheckTA");
+	btEvent.addEventListener("click", reCheck, false);
+};
+
+(function() {
+	'use strict';
+
+	totalAmount();
+})();
